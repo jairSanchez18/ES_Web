78b44382c7ab37bd9aa60518d6eac96af3066178
Carlos Fernandez <carlos.fernandez3@utp.ac.pa>
2022-10-08 19:06:40 -0500

    Correccion de la pantalla de listado de asistencia

--- view/Listado_de_asistencia.php
+++ view/Listado_de_asistencia.php
@@ -14,59 +14,50 @@
     <link rel="stylesheet" href="../resources/styles/estilos personalizados/dev_cefv.css">
 </head>
 
-<body>
-    <h1>1LS-131</h1>
-    <h2>fecha:<input name="date" type="date"></h2>
-    <button>Editar</button><button>Guardar</button>
+<body class="back-style">
+
+    <h1 class="subtext-style">1LS-131</h1>
+
+    <div class="dropdown m-2">
+            <a class="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Seleccione la Fecha
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">02/10/2022</a></li>
+              <li><a class="dropdown-item" href="#">10/11/2022</a></li>
+              <li><a class="dropdown-item" href="#">01/02/2022</a></li>
+            </ul>
+          </div>
+    <div>
+        <button class="btn-style">Editar</button>
+   
+        <button class="btn-estilo">Guardar</button>
+    </div>  
+
     <section class="home"></section>
-    <table id="tabla" style="width: 80%;" class="in-flex">
+    <table id="tabla" style="width: 100%;" class="in-flex">
         <tr>
-            <th>Nombre</th><th>Apellido</th><th>Correo</th><th>Cédula</th><th>Asistencia</th><th>Porcentaje</th></tr>
+            <th>Nombre</th><th>Apellido</th><th>Correo</th><th>Cédula</th><th>Asistencia</th><th>Porcentaje</th><th>Observaciones</th></tr>
 
             <tr><td>Carlos</td><td>Fernández</td><td>carlos.fernandez3@utp.ac.pa</td><td>8-976-518</td>
-            <td>
-                <img src="https://emojitool.com/img/apple/ios-15.4/large-green-circle-3377.png">
-            </td>
-            <td>
-                5%
-            </td>
-        </tr>
+            <td><img src="https://emojitool.com/img/apple/ios-15.4/large-green-circle-3377.png"></td>
+            <td> 5%</td><td>El estudiante no se bañó</td></tr>
 
         <tr><td>Carlos</td><td>Fernández</td><td>carlos.fernandez3@utp.ac.pa</td><td>8-976-518</td>
-            <td>
-                <img src="https://emojitool.com/img/apple/ios-15.4/large-green-circle-3377.png">
-            </td>
-            <td>
-                5%
-            </td>
-        </tr>
+        <td><img src="https://emojitool.com/img/apple/ios-15.4/large-green-circle-3377.png"></td>
+            <td> 5%</td><td>El estudiante no se bañó</td></tr>
 
         <tr><td>Carlos</td><td>Fernández</td><td>carlos.fernandez3@utp.ac.pa</td><td>8-976-518</td>
-            <td>
-                <img src="https://emojitool.com/img/apple/ios-15.4/large-green-circle-3377.png">
-            </td>
-            <td>
-                5%
-            </td>
-        </tr>
+        <td><img src="https://emojitool.com/img/apple/ios-15.4/large-green-circle-3377.png"></td>
+            <td> 5%</td><td>El estudiante no se bañó</td></tr>
 
         <tr><td>Carlos</td><td>Fernández</td><td>carlos.fernandez3@utp.ac.pa</td><td>8-976-518</td>
-            <td>
-                <img src="https://emojitool.com/img/apple/ios-15.4/large-green-circle-3377.png">
-            </td>
-            <td>
-                5%
-            </td>
-        </tr>
+        <td><img src="https://emojitool.com/img/apple/ios-15.4/large-green-circle-3377.png"></td>
+            <td> 5%</td><td>El estudiante no se bañó</td></tr>
 
         <tr><td>Carlos</td><td>Fernández</td><td>carlos.fernandez3@utp.ac.pa</td><td>8-976-518</td>
-            <td>
-                <img src="https://emojitool.com/img/apple/ios-15.4/large-green-circle-3377.png">
-            </td>
-            <td>
-                5%
-            </td>
-        </tr>
+            <td><img src="https://emojitool.com/img/apple/ios-15.4/large-green-circle-3377.png"></td>
+            <td> 5%</td><td>El estudiante no se bañó</td></tr>
 
     </table>
 </section>
